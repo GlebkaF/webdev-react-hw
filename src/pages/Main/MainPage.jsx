184f74e9c713c535818a246863fa50ec55ca52b3
Gleb Fokin <g.fokin@skyeng.ru>
2023-08-16 13:02:04 +0700

    deploy to pages

--- src/pages/Main/MainPage.jsx
+++
@@ -1,8 +0,0 @@
-import Tracklist from '../../components/Tracklist'
-import { useGetMainPlaylistQuery } from '../../api/playlist'
-
-export default function MainPage() {
-  const { data, error, isLoading } = useGetMainPlaylistQuery()
-
-  return <Tracklist tracks={data} error={error} loading={isLoading} />
-}
